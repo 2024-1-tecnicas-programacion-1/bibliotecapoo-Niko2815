7d74b5c89daf6fa56ab0cb74c5e7b9180d9d0b0b
Niko2815 <146888012+Niko2815@users.noreply.github.com>
2024-04-29 20:48:48 -0500

    Update Biblioteca.java

--- src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
+++ src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
@@ -1,5 +1,38 @@
 package com.mycompany.bibliotecapoo;
 
-public class Biblioteca {
-    // TODO: Aquí va tu código
-}
+import java.util.LinkedList;
+
+public class Biblioteca {//abre Class
+LinkedList<Libro> libros = new LinkedList();
+boolean leido = false;
+
+public void registrarLibro(Libro libro){//abre registrarLibro
+libros.add(libro);
+}//cierra registrarLibro
+
+public String buscarLibro (String palabraBusqueda){//abre buscar libro
+    for (Libro i: libros) {//abre for
+        if ((i.getTitulo().equals(palabraBusqueda)) || i.getAutor().equals(palabraBusqueda) || i.getGenero().equals(palabraBusqueda)) {//abre if
+          
+            return palabraBusqueda;  
+        }//cierra if
+ 
+    }//cierra for
+
+return null;
+}//cierra buscar libro
+public void mostrarLibros(){//abre mostrar libros
+    for (Libro libro : libros) {//cierra 2for
+        System.out.println(libro.toString());
+    }//cierra 2for
+
+}//cierra mostarlibros
+
+public void mostrarLibrosNoLeidos(){//abre libros no leidos
+    for (Libro libro : libros) {//abre for
+        if (!libro.leido) {//abre if
+            System.out.println(libro.mostrarInformacion());
+        }//cierra id
+    }//cierra for
+  }//ciera libros no leidos
+}//cierra Class
